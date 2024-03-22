d66bfde654c264443479a19635c44f6b51ab5460
Axl-Gomez-2909 <148070790+Axl-Gomez-2909@users.noreply.github.com>
2024-03-21 18:09:26 -0600

    Axel Gomez

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import TaskList from './components/TaskList';
+import AddTaskForm from './components/AddTaskForm';
 
-function App() {
+const App = () => {
+  const [tasks, setTasks] = useState([
+    { id: 1, title: 'Hacer la compra', status: 'todo' },
+    { id: 2, title: 'Preparar la cena', status: 'in-progress' },
+    { id: 3, title: 'Llamar al médico', status: 'done' }
+  ]);
+
+  const addTask = (title) => {
+    const newTask = {
+      id: Math.random(),
+      title: title,
+      status: 'todo'
+    };
+    setTasks([...tasks, newTask]);
+  };
+
+  const deleteTask = (taskId) => {
+    const updatedTasks = tasks.filter(task => task.id !== taskId);
+    setTasks(updatedTasks);
+  };
+
+  const updateTask = (taskId) => {
+    const updatedTasks = tasks.map(task => {
+      if (task.id === taskId) {
+        if (task.status === 'todo') {
+          return { ...task, status: 'in-progress' };
+        } else if (task.status === 'in-progress') {
+          return { ...task, status: 'done' };
+        } else {
+          return { ...task, status: 'todo' };
+        }
+      }
+      return task;
+    });
+    setTasks(updatedTasks);
+  };
+
+  const deleteAllTasks = () => {
+    setTasks([]); // Elimina todas las tareas estableciendo el estado de las tareas como un array vacío
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="mt-4 mb-3">Lista de Tareas</h1>
+      <AddTaskForm onAdd={addTask} />
+      <hr />
+      {/* Agrega un botón para eliminar todas las tareas */}
+      <button className="btn btn-danger mb-3" onClick={deleteAllTasks}>Eliminar todas las tareas</button>
+      <TaskList tasks={tasks} onDelete={deleteTask} onUpdate={updateTask} />
     </div>
   );
-}
+};
 
 export default App;
